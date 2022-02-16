2ccde168ec8593695560f4b0e2eb58031c06123f
Steven Kelly <stevenkelly418@gmail.com>
2022-02-16 10:20:57 +0000

    => Adding initial base project to the repository.

---
+++ WPF_DB_Test1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_DB_Test1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Exercise 1 - Code to retrieve customer data from the database
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                select c.CompanyName;
+
+            Ex1ListBox.ItemsSource = query.ToList();
+        }
+    }
+}
